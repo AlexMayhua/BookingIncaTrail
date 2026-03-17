452b1425c27ca473fb5b6175eca71e9b884cfb66
Alexmayhua <alexmayhuaquispe@gmail.com>
2026-03-17 09:49:01 -0500

    Refactor _app.js and _document.js: improve code formatting, remove unused favicon links, and streamline imports for better readability.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,60 +1,47 @@
-import '../styles/globals.css'
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import '../styles/react-datepicker.css'
-import '../styles/custom-tailwind.css'
-import '../styles/cal.css'
-import '../styles/hero.css'
-import '../styles/tours.css'
-import '../styles/categories-section.css'
-import { initFacebookPixel } from '../lib/facebookPixel';
-import { initTikTokPixel } from '../lib/tiktokPixel';
-import Script from 'next/script'
-import { GTM_ID, pageview } from '../lib/gtm'
+import '../styles/globals.css';
+import '../styles/react-datepicker.css';
+import '../styles/custom-tailwind.css';
+import '../styles/cal.css';
+import '../styles/hero.css';
+import '../styles/tours.css';
+import '../styles/categories-section.css';
+import 'keen-slider/keen-slider.min.css';
+import { GoogleTagManager } from '@next/third-parties/google';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+
+import { GTM_ID } from '../lib/gtm';
 import { DefaultSeo } from 'next-seo';
 import { DefaultSeo as DefaultSeoConfig } from '../../next-seo.config.js';
-import { DataProvider } from '../store/GlobalState'
+
+import { DataProvider } from '../store/GlobalState';
 import Layout from '../layout/Layout';
-import AdminLayout from '../layout/AdminLayout';
-import 'keen-slider/keen-slider.min.css';
 
 function MyApp({ Component, pageProps }) {
-  const { pathname } = useRouter();
-  const router = useRouter()
-  const lang = pathname.startsWith("/es") ? "es-ES" : "en";
-  
-  // Determinar si la página es del admin
+  const router = useRouter();
+  const { pathname } = router;
+
+  const lang = pathname.startsWith('/es') ? 'es-ES' : 'en';
   const isAdminPage = pathname.startsWith('/admin');
-  const LayoutComponent = isAdminPage ? AdminLayout : Layout;
-  
+  const LayoutComponent = isAdminPage ? ({ children }) => children : Layout;
+
   useEffect(() => {
     document.documentElement.lang = lang;
   }, [lang]);
 
   useEffect(() => {
-    // Inicializa ambos píxeles
-    initFacebookPixel();
-    initTikTokPixel();
-
     const handleRouteChange = (url) => {
-      // Rastrear la vista de página para ambos píxeles
-      if (typeof window.fbq !== 'undefined') {
-        window.fbq('track', 'PageView');
-      }
-
-      if (typeof window.ttq !== 'undefined') {
-        window.ttq.push(['track', 'PageView']);  // Event type for TikTok
-      }
-
-      if (typeof window !== 'undefined') {
-        pageview(url); // Google Tag Manager pageview
-      }
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'virtual_pageview',
+        page: url,
+      });
     };
 
-    // Escuchar los cambios de ruta
     router.events.on('routeChangeComplete', handleRouteChange);
 
-    // Limpiar el evento cuando el componente se desmonte
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
@@ -63,31 +50,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...DefaultSeoConfig} />
-
-      {/* Solo cargar GTM si el ID está definido */}
-      {GTM_ID && (
-        <Script
-          id="gtm-head"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
-          }}
-        />
-      )}
-
+      <GoogleTagManager gtmId={GTM_ID} />
       <DataProvider>
         <LayoutComponent>
           <Component {...pageProps} />
         </LayoutComponent>
       </DataProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
