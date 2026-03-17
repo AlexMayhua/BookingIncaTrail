0339c1f6ce38121b478e022c7b2246b126fbec38
Alexmayhua <alexmayhuaquispe@gmail.com>
2026-03-17 09:49:48 -0500

    Refactor GlobalState: streamline state management by removing unused auth and users properties, and improve code formatting for better readability.

--- src/store/GlobalState.js
+++ src/store/GlobalState.js
@@ -1,49 +1,32 @@
-import { createContext, useReducer, useEffect } from 'react'
-import reducers from './Reducers'
-import { getData } from '../utils/fetchData'
+import { createContext, useReducer, useEffect } from 'react';
+import reducers from './Reducers';
 
+export const DataContext = createContext();
 
-export const DataContext = createContext()
+export const DataProvider = ({ children }) => {
+  const initialState = {
+    notify: {},
+    cart: [],
+    modal: [],
+    orders: [],
+  };
 
+  const [state, dispatch] = useReducer(reducers, initialState);
+  const { cart } = state;
 
-export const DataProvider = ({children}) => {
-    const initialState = { 
-        notify: {}, auth: {}, cart: [], modal: [], orders: [], users: []
-    }
+  useEffect(() => {
+    const ecommerce_next = JSON.parse(localStorage.getItem('ecommerce_next'));
 
-    const [state, dispatch] = useReducer(reducers, initialState)
-    const { cart, auth } = state
+    if (ecommerce_next) dispatch({ type: 'ADD_CART', payload: ecommerce_next });
+  }, []);
 
-    useEffect(() => {
-        const firstLogin = localStorage.getItem("firstLogin");
-        if(firstLogin){
-            getData('auth/accessToken').then(res => {
-                if(res.err) return localStorage.removeItem("firstLogin")
-                dispatch({ 
-                    type: "AUTH",
-                    payload: {
-                        token: res.access_token,
-                        user: res.user
-                    }
-                })
-            })
-        }
-    
-    },[])
+  useEffect(() => {
+    localStorage.setItem('ecommerce_next', JSON.stringify(cart));
+  }, [cart]);
 
-    useEffect(() => {
-        const ecommerce_next = JSON.parse(localStorage.getItem('ecommerce_next'))
-
-        if(ecommerce_next) dispatch({ type: 'ADD_CART', payload: ecommerce_next })
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem('ecommerce_next', JSON.stringify(cart))
-    }, [cart])
-
-    return(
-        <DataContext.Provider value={{state, dispatch}}>
-            {children}
-        </DataContext.Provider>
-    )
-}
+  return (
+    <DataContext.Provider value={{ state, dispatch }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
