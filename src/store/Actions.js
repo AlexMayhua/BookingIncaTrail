e072023f2104c07ad6ca7e2798ecf254e0518a11
Alexmayhua <alexmayhuaquispe@gmail.com>
2026-03-17 09:46:02 -0500

    Refactor action types and reducers: remove unused AUTH and USERS actions, and improve code formatting for better readability.

--- src/store/Actions.js
+++ src/store/Actions.js
@@ -1,18 +1,16 @@
 export const ACTIONS = {
-    NOTIFY: 'NOTIFY',
-    AUTH: 'AUTH',
-    ADD_CART: 'ADD_CART',
-    ADD_MODAL: 'ADD_MODAL',
-    ADD_ORDERS: 'ADD_ORDERS',
-    ADD_USERS: 'ADD_USERS',
-}
+  NOTIFY: 'NOTIFY',
+  ADD_CART: 'ADD_CART',
+  ADD_MODAL: 'ADD_MODAL',
+  ADD_ORDERS: 'ADD_ORDERS',
+};
 
 export const deleteItem = (data, id, type) => {
-    const newData = data.filter(item => item._id !== id)
-    return ({ type, payload: newData})
-}
+  const newData = data.filter((item) => item._id !== id);
+  return { type, payload: newData };
+};
 
 export const updateItem = (data, id, post, type) => {
-    const newData = data.map(item => (item._id === id ? post : item))
-    return ({ type, payload: newData})
-}
+  const newData = data.map((item) => (item._id === id ? post : item));
+  return { type, payload: newData };
+};
